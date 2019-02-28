f7720bbaf53a232105b74858bcfb4439368c32fd
Keyrr Johnmel C. Perino <keyrr@sym.ph>
2019-02-28 18:00:27 +0800

    remove print

--- libraries/helpers/strings.py
+++ libraries/helpers/strings.py
@@ -6,5 +6,3 @@ def generate_random_string(n=8):
     """Generate random string could be used for IDs, etc"""
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(n))
-
-print generate_random_string(n=20)
